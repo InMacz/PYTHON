63c758d2a32b7568cd9ba236cebf6c24d136f5d1
InMacz <inmacz321@gmail.com>
2026-04-02 18:14:25 -0300

    Create ex18.py

---
+++ lista2/ex18.py
@@ -0,0 +1,8 @@
+dia = int(input("Informe o dia: "))
+if dia > 31 or dia < 0:
+    print("Data inválida!")
+
+mes = int(input("Informe o mes: "))
+if mes > 12 or mes < 0:
+    print("Data inválida!")
+ano = int(input("Informe o ano: "))
