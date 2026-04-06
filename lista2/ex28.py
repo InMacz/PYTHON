e1e62c5a43a6f335f9d2a81c8ac3c41ac12045bb
InMacz <inmacz321@gmail.com>
2026-04-06 15:11:42 -0300

    Update ex28.py

--- lista2/ex28.py
+++ lista2/ex28.py
@@ -1,18 +1,46 @@
-kgMorango = float(input("Digite em Kg a quantidade de Morangos: "))
-kgMaca = float(input("Digite em Kg a quantidade de Maçãs: "))
-if kgMorango <= 5:
-    precoMorango = kgMorango * 2.5
+tipoCarne = (input("Digite o tipo da carne, File Duplo, Alcatra ou Picanha: F / A / P: "))
+if tipoCarne.upper() != "F" and tipoCarne.upper() != "A" and tipoCarne.upper() != "P":
+    print("Carne inválida!")
+    exit()
+
+kgCarne = float(input("Digite em Kg da carne: "))
+
+if tipoCarne.upper() == "F":
+    tipoCarneTexto = "File Duplo"
+    if kgCarne <= 5:
+        precoCarne = kgCarne * 4.9
+    else:
+        precoCarne = kgCarne * 5.8
+
+elif tipoCarne.upper() == "A":
+    tipoCarneTexto = "Alcatra"
+    if kgCarne <= 5:
+        precoCarne = kgCarne * 5.9
+    else:
+        precoCarne = kgCarne * 6.8
+
+elif tipoCarne.upper() == "P":
+    tipoCarneTexto = "Picanha"
+    if kgCarne <= 5:
+        precoCarne = kgCarne * 6.9
+    else:
+        precoCarne = kgCarne * 7.8
+    
+
+tipoPagamento = input("Vai pagar com cartão Tabajara? S / N: ")
+if tipoPagamento.upper() == "S":
+    desconto = (precoCarne*5)/100
+    precoDesconto = precoCarne-desconto
+elif tipoPagamento.upper() == "N":
+    desconto = 0
+    precoDesconto = precoCarne
 else:
-    precoMorango = kgMorango * 2.2
-if kgMaca <= 5:
-    precoMaca = kgMaca * 1.8
-else:
-    precoMaca = kgMaca * 1.5
+    print("Digite S ou N!")
+    exit()
 
-totalPreco = precoMaca + precoMorango
-totalKg = kgMaca + kgMorango
 
-if totalPreco > 25 or totalKg > 8:
-    totalPreco = totalPreco - (totalPreco*10)/100
 
-print(f"Valor á ser pago será de: R$ {totalPreco:.2f}")
+print(f"Tipo da carne: {tipoCarneTexto}, quantidade: {kgCarne} KG, preço total: R${precoCarne:.2f}, Tipo de pagamento: {'Cartão Tabajara' if tipoPagamento.upper() == 'S' else 'Outro'}, desconto: R${desconto:.2f}, valor a pagar: {precoDesconto:.2f}")
+
+
+
